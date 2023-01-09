ac61df20ef7f870284b44b3f586a006bd68b8c7c
promilaminz <promilaminz07@gmail.com>
2023-01-09 12:36:22 +0530

    77. Combinations

--- LeetCode/Medium/77. Combinations/Combination.cs
+++ LeetCode/Medium/77. Combinations/Combination.cs
@@ -8,5 +8,29 @@ namespace DSAndAlgo.LeetCode.Medium._77._Combinations
 {
     internal class Combination
     {
+
+            public IList<IList<int>> Combine(int n, int k)
+            {
+                List<IList<int>> result = new List<IList<int>>();
+                Subsets(1, n, new List<int>(), result, k);
+                return result;
+            }
+            public void Subsets(int start, int end, List<int> currentComnbination, List<IList<int>> result, int size)
+            {
+                if (currentComnbination.Count == size)
+                {
+                    result.Add(new List<int>(currentComnbination));
+                    return;
+                }
+                for (int i = start; i <= end; i++)
+                {
+                    currentComnbination.Add(i);
+                    Subsets(i + 1, end, currentComnbination, result, size);
+                    currentComnbination.RemoveAt(currentComnbination.Count - 1);
+                }
+
+
+            }
+
     }
 }
